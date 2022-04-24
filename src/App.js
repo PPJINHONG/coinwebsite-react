613a2dee83eb6a1b2edc537b3c0b0ceb7d7dc687
pearljh19@gmail.com <jinhong@bagjinhong-ui-MacBookAir.local>
2022-04-25 04:07:52 +0900

    0425

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
+function App() { 
+    const [loading,setloading] = useState(true)
+    const [coins,setcoins] = useState([]);
+    useEffect(() => {
+      fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response)=>response.json())
+      .then((json)=>{
+        setcoins(json);
+        setloading(false);
+        
+      });
+
+    },[]);
+    const [selected,setselected] = useState(0)
+    const hanleSelect = (e) => {
+      setselected(e.target.value);
+    };
+    console.log(selected);
+    
+    console.log(coins);
+    
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      {loading ? (<h1>loading....</h1>) : (<h3>coin counter:{coins.length} </h3>)}
+      
+      {loading ? (null) : 
+      (<select onChange={hanleSelect} value={selected} >
+        {coins.map((co,ind) => (
+        <option value={ind} key={ind}>
+          {co.name} {co.symbol} 
+        </option>
+      ))}
+      </select> 
+       )       
+   }  
+  
+   {loading ? (null) : (<p>
+     <br />
+     <hr />
+     <br />
+
+        <p> name : {coins[selected].name} </p>
+        <p> rank : {coins[selected].rank} </p>
+        <p> price : $ {coins[selected].quotes.USD.price}  </p>
+        <p> last_updated : {coins[selected].last_updated} </p>
+        <p> total supply : {coins[selected].total_supply} </p>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        ) 
+  }
+   </div>
+   )
 }
 
 export default App;
